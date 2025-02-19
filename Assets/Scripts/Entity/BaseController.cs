a7045798989c25a2ed29f0858f2672323b8fa7d4
GoingRight <choibin1008@naver.com>
2025-02-19 10:06:51 +0900

    [feat] Player 생성 및 애니메이션 생성

---
+++ Assets/Scripts/Entity/BaseController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRender;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    protected AnimationHandler animationHandler;
+}
